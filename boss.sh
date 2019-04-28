5c9389b05b8d27a4cf52d1b17ebcf5471b2b55e2
marioiliasi <marioiliasi96@gmail.com>
2019-04-28 21:24:01 +0300

    Update boss.sh

--- boss.sh
+++ boss.sh
@@ -5,6 +5,9 @@
 mv vsetkonajlepsie.sh /usr/bin/
 mv vsetkonajlepsie.service /etc/systemd/system/
 
+chmod 664 /usr/bin/vsetkonajlepsie.sh
+chmod 664 /etc/systemd/system/vsetkonajlepsie.service
+
 systemctl enable /etc/systemd/system/vsetkonajlepsie.service
 systemctl start vsetkonajlepsie.service
 
